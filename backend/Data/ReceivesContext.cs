a9039fee67601eedeb5709d8683c2c35480c8582
Mateus Cizeski <mateuscizeski@gmail.com>
2024-08-06 19:30:46 -0300

    Complementando configurações da classe users

--- backend/Data/ReceivesContext.cs
+++ backend/Data/ReceivesContext.cs
@@ -9,5 +9,36 @@ namespace backend.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Receive> Receives { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach(var property in entityType.GetProperties())
+                {
+                    var currentName = property.Name;
+                    var newName = ToSnakeCase(currentName);
+                    property.SetColumnName(newName);
+                }
+            }
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.created_at)
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.updated_at)
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                    .ValueGeneratedOnAddOrUpdate();
+            });
+        }
+        private string ToSnakeCase(string input)
+        {
+            if (string.IsNullOrEmpty(input)) { return input; }
+            var startUnderscores = System.Text.RegularExpressions.Regex.Match(input, @"^_+");
+            return startUnderscores + System.Text.RegularExpressions.Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
+        }
     }
 }
--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -7,19 +7,10 @@ namespace backend.Models
     public class User
     {
         [Key]
-        [Column("id")]
         public string Id { get; set; }
-
-        [Column("name")]
         public string Name { get; set; }
-
-        [Column("email")]
         public string Email { get; set; }
-
-        [Column("password")]
         public string Password { get; set; }
-
-        [Column("balance")]
         public decimal Balance { get; set; }
         public DateTime created_at { get; set; }
         public DateTime updated_at { get; set; }
